93829a031dd32ab2c16fa3c0187381f0b5227bc9
raju singho <53114843+rajuswesust@users.noreply.github.com>
2023-03-05 13:44:57 +0600

    Update server.js

--- server.js
+++ server.js
@@ -6,12 +6,18 @@ const fs = require("fs");
 
 const { createReadStream } = require('fs');
 
-function handleLog(msg) {
-    fs.appendFile('log.txt', msg + "\n\n", function (err) {
+function handleLog(req, statusCode, msg) {
+
+    let time = new Date().toDateString();
+    let { method } = req;
+    let clientIp = req.socket.remoteAddress;
+    let requestUrl = req.url;
+    let logMessage = time + "\t" + method + "\t" + statusCode + "\t" + clientIp + "\t" + requestUrl + "\t" + msg + "\n";
+    fs.appendFile('log.txt', logMessage, function (err) {
         if (err) {
             console.log(err);
         } else {
-            console.log("successfully logged");
+            console.log("successfully logged")
         }
     })
 };
@@ -43,111 +49,114 @@ function getContentType(ext) {
     }
 }
 
-const server = http.createServer((request, response) => {
-    console.log(`Server has started at  http://localhost:${PORT}/`);
+function readDirectory(filePath, url) {
+    if(url == "/")
+        url = "";
+    let s = `<h1>List of Files:</h1>`;
+    try {
+        s += `<ol>`
+        const res = fs.readdirSync(filePath);
+        res.forEach(file => {
+            s += `<li><p> <a style="text-decoration:none" href= "http://localhost:${PORT}${url}/${file}">${file}</a></li>`
+        });
+        s += `</ol`;
+    } catch (err) {
+        return [0, err];
+    }
+    return [1, s];
+}
 
+const server = http.createServer((request, response) => {
     const { headers, method, url } = request;
     console.log(method + ", " + url);
 
     if (method !== "GET") {
         response.statusCode = 405;
-        response.end(
-            "Operation is not supported!"
-        )
+        response.end("Method Not Allowed");
+        handleLog(req, response.statusCode, "Method Not Allowed");
     };
 
     const filePath = __dirname + url;
+    console.log("directory: " + __dirname);
     console.log("file path : " + filePath);
-
-    let s = "", ok = 0;
-    let ans = new Promise((resolve, reject) => {
-        fs.readdir(__dirname, (err, files) => {
-            if (err) {
-                reject(err);
-            }
-            else {
-                files.forEach(file => {
-                    s = s + file + "\n";
-                    // console.log("->" + file);
-                });
-                resolve(s);
-            }
-        });
-    });
-
-    ans.then(x => {
-        console.log(x);
-        if (url == '/') {
-
-            handleLog("list of files are requested");
-            handleLog(x);
-
-            response.end(x);
-        }
-    }).catch(err => {
-        console.log(err);
-    });
-
 
     let arr = url.split('/');
     let reqContent = arr[arr.length - 1];
-
     console.log("requested : ", reqContent);
 
-
-  if (!fs.existsSync(filePath)) {
+    if (!fs.existsSync(filePath)) {
         response.writeHead(404, {
             "Content-Type": "text/plain"
         });
-        handleLog(`${reqContent} file does not exists`);
+        handleLog(request, response.statusCode, `${reqContent} file does not exists`);
         response.end(`${reqContent} file does not exists\n` + "404 Not Found");
         return;
     }
 
-    fs.access(filePath, fs.constants.R_OK,function (err) {
+    fs.stat(filePath, (err, stats) => {
         if (err) {
-            response.writeHead(404, {
-                "Content-Type": "text/plain"
-            });
-            response.end(`${reqContent} file cannot be accessed`);
+            response.statusCode = 505;
+            handleLog(request, 500, " Internal Server Error");
         } else {
-            let ext = reqContent.split('.')[1];
+            if (stats.isFile()) {
+                console.log("#requested url is a file");
 
-            console.log("extension: " + ext);
+                fs.access(filePath, fs.constants.R_OK, function (err) {
+                    if (err) {
+                        response.writeHead(404, {
+                            "Content-Type": "text/plain"
+                        });
+                        response.end(`${reqContent} file cannot be accessed`);
+                        handleLog(request, 403, "Forbidden");
+                    } else {
+                        let ext = reqContent.split('.')[1];
 
-            const contentType = getContentType(ext);
+                        console.log("extension: " + ext);
 
-            console.log("con-type: ", contentType);
+                        const contentType = getContentType(ext);
 
-            if (ext == "mp4") {
-                response.writeHead(200, {
-                    'Content-Type': 'video/mp4'
+                        console.log("con-type: ", contentType);
+
+                        response.writeHead(200, {
+                            'Content-Type': contentType
+                        });
+
+                        const fileStream = createReadStream(filePath);
+                        fileStream.on('error', (e) => {
+                            console.log(e);
+                            handleLog(request, 500, " Internal Server Error");
+                        })
+                            .pipe(response)
+                            .on('error', (err) => {
+                                console.log(e);
+                                handleLog(request, 500, " Internal Server Error");
+
+                            });
+
+                        fileStream.on('close', () => {
+                            response.statusCode = 200;
+                            handleLog(request, 200, "OK");
+                        });
+
+                    };
                 });
-                createReadStream(filePath).pipe(response);
-            } else {
 
-                // Setting the headers
-                response.writeHead(200, {
-                    "Content-Type": contentType
-                });
-
-                // Reading the file
-                fs.readFile(filePath,
-                    function (err, content) {
-                        if (err) {
-                            response.end(
-                                `Error while reading ${reqContent} file`
-                            );
-                            return;
-                        }
-                        // Serving the image
-                        response.end(content);
-                    });
+            } else if (stats.isDirectory()) {
+                let fileList = readDirectory(filePath, url);
+                if(!fileList[0]) {
+                    response.statusCode = 500;
+                    res.end("Error while reading the directory \n Internal server error");
+                    handleLog(request, 500, " Internal Server Error");
+                    return;
+                }
+                console.log("#requested url is a a directory");
+                console.log(fileList);
+                response.end(fileList[1]);
+                handleLog(request, 200, "OK");
+                return;
             }
-        };
+        }
     });
-
-    // response.end("This is a server...");
 }).listen(PORT, () => {
     console.log(`Server has started at http://localhost:${PORT}`);
 });
